cbf7cf854f06df2a00c2e4d4ddb58a64a11bcabc
meiskam <meiskam@gmail.com>
2015-03-11 14:45:50 -0400

    Added DTR command structure

--- src/main/java/org/shininet/bukkit/deathtoraidable/DeathToRaidable.java
+++ src/main/java/org/shininet/bukkit/deathtoraidable/DeathToRaidable.java
@@ -1,7 +1,21 @@
 package org.shininet.bukkit.deathtoraidable;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class DeathToRaidable extends JavaPlugin {
 
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (!cmd.getName().equalsIgnoreCase("DeathToRaidable")) {
+            return false;
+        }
+        if (args.length == 0) {
+        	sender.sendMessage("Usage: /" + label + " <faction>"); //TODO .. own faction without params?
+            return true;
+        }
+    	sender.sendMessage("{Faction}'s DTR is {DTR}"); //TODO
+    	return true;
+    }
 }
