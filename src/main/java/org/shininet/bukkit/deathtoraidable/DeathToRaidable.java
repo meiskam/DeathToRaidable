a3f2f7a073fb646cc88512e158b0bcc438ee35bf
meiskam <meiskam@gmail.com>
2015-03-11 14:59:55 -0400

    Added PlayerDeathEvent

--- src/main/java/org/shininet/bukkit/deathtoraidable/DeathToRaidable.java
+++ src/main/java/org/shininet/bukkit/deathtoraidable/DeathToRaidable.java
@@ -1,10 +1,21 @@
 package org.shininet.bukkit.deathtoraidable;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class DeathToRaidable extends JavaPlugin {
+public class DeathToRaidable extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        Bukkit.getPluginManager().registerEvents(this, this);
+    }
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
@@ -17,5 +28,15 @@ public class DeathToRaidable extends JavaPlugin {
         }
     	sender.sendMessage("{Faction}'s DTR is {DTR}"); //TODO
     	return true;
+    }
+
+    @EventHandler(priority=EventPriority.NORMAL)
+    public void onPlayerDeath(PlayerDeathEvent event)
+    {
+        Player player = event.getEntity();
+        if (!(player.hasPermission("deathtoraidable.enable"))) {
+        	return;
+        }
+    	// TODO change DTR if in faction
     }
 }
